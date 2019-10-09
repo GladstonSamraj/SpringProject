9c1f643d9b8e02b02cf7ae23e72773b44cbdbb2b
Gladstom Samraj <gladstonsamraj@gmail.com>
2019-10-09 23:17:25 +0530

    Application Properties

--- src/main/java/com/gs/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/gs/springbootdemo/SpringbootdemoApplication.java
@@ -1,20 +1,47 @@
 package com.gs.springbootdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RestController
-public class SpringbootdemoApplication {
+public class SpringbootdemoApplication implements ApplicationRunner {
+
+	@Value("${spring.application.name}")
+	private String name;
+
+	@Autowired
+	RestTemplate restTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootdemoApplication.class, args);
 	}
 
-	@RequestMapping(value = "/")
+	@RequestMapping(value = "/name")
+	public String name() {
+		return name;
+	}
+
+	@RequestMapping(value = "/hello")
 	public String hello() {
 		return "Hello World";
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("Hello World from Application Runner...");
 	}
 }
